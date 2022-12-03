82dd646bfdf8f3a995ff5faca7f5716803462597
qwexzy <65239981+qwexzy@users.noreply.github.com>
2022-12-03 19:34:06 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,9 +2,8 @@ fx_version 'cerulean'
 game 'gta5'
 
 version '0.1.3'
-author 'Reece Stokes <hagen@hyena.gay>'
-description 'FiveM resource for ACE-specific blacklisting.'
-repository 'https://github.com/MrGriefs/dynamicblacklist'
+author 'HUGO LEAKS'
+description 'JEBACKI'
 
 shared_scripts {
   'ammotypes.lua',
